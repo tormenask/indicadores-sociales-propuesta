dcb6e687859097c36f9757f7e015b5facb6b8f6f
tormenask <santiagoballa2003@gmail.com>
2024-11-30 12:18:56 -0500

    added index

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "model/link.php";
+
+require_once "controller/template.php";
+require_once "controller/link.php";
+
+$template = new TemplateControllerBack();
+$template->templateBack();
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "models/links.php";
+
+require_once "controllers/template.php";
+require_once "controllers/links.php";   
+
+$template = new TemplateController();
+$template -> template();
